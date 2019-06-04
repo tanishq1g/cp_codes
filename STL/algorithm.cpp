dacfa4afd3e909f867e6b10783971e7d31330076
Tanishq Gupta <tanishq.gupta@iiitb.org>
2019-06-04 23:56:07 +0530

    Add files via upload

---
+++ STL/algorithm.cpp
@@ -0,0 +1,9 @@
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    min(x,y)
+    // x and y can only be integers and not overloaded functions
+    return 0;
+}
